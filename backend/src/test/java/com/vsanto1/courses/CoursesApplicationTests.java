0cec66faa5fe2c821d5c13ca5946153e7b40373b
vsantos1 <vinicius.shyn2@gmail.com>
2023-02-01 16:09:04 -0300

    setup project dockerized

---
+++ backend/src/test/java/com/vsanto1/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsanto1.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
